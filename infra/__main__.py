9866105e01eec0934b3f3be15fdd3949e8c2cb9d
Aleksandar <aleksandar.jovanov.1995@gmail.com>
2026-07-01 14:44:51 +0200

    Initial IaC planning and setup

---
+++ infra/__main__.py
@@ -0,0 +1,40 @@
+"""A Google Cloud Python Pulumi program"""
+
+import pulumi
+import pulumi_gcp as gcp
+
+repo = gcp.artifactregistry.Repository(
+    "c31h64-threewhitetowers-repo",
+    location="europe-west1",
+    repository_id="c31h64-twt-repo",
+    format="DOCKER"
+)
+
+image_name = repo.name.apply(
+    lambda name: f"europe-west1-docker.pkg.dev/{gcp.config.project}/name/axum-demo-hw:latest"
+)
+
+service = gcp.cloudrun.Service(
+    "c31h64-twt-axum-demo-hw-service",
+    location="europe-west1",
+    template=gcp.cloudrun.ServiceTemplateArgs(
+        spec=gcp.cloudrun.ServiceTemplateSpecArgs(
+            comtainers=[
+                gcp.cloudrun.ServiceTemplateSpecContainerArgs(
+                    image=image_name,
+                )
+            ]
+        )
+    )
+)
+
+
+iam = gcp.cloudrun.IamMember(
+    "c31h64-twt-public-access",
+    service = service.name,
+    location = service.location,
+    role = "roles/run.invoker",
+    memer = "allUsers"
+)
+
+pulumi.export("url", service.statuses.apply(lambda s: s[0].url))
