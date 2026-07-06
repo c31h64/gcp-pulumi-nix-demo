a22695c80ef678127884a0ee1a566ae58687e8b1
Aleksandar <aleksandar.jovanov.1995@gmail.com>
2026-07-06 12:38:02 +0200

    Update the frontend to show some basic UI over the adjudicate API

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AdjudicateOutcome } from '../types';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,37 @@ import { HttpClient } from '@angular/common/http';
 })
 export class App {
   message = signal<string>('');
+
+  has_outcome = signal<boolean>(false);
+  arguments_side_a = signal<string>('');
+  arguments_side_b = signal<string>('');
+  winner_side = signal<string>('');
+  winner_probability = signal<number>(-1.0);
+  compromise_solution = signal<string>('');
+
   private http = inject(HttpClient);
+
+  adjudicate() {
+    this.http.post<AdjudicateOutcome>('/adjudicate', {
+      'problem_text':  document.getElementById("problemStatementWidget")?.textContent,
+      'side_a_text': document.getElementById("sideAWidget")?.textContent,
+      'side_b_text': document.getElementById("sideBWidget")?.textContent,
+    }).subscribe({
+      next: (res: AdjudicateOutcome) => {
+        this.has_outcome.set(true);
+        this.arguments_side_a.set(res.arguments_side_a);
+        this.arguments_side_b.set(res.arguments_side_b);
+        this.winner_side.set(res.winner_side);
+        this.winner_probability.set(res.winner_probability);
+        this.compromise_solution.set(res.compromise_solution);
+        this.message.set(JSON.stringify(res, null, 2));
+      },
+      error: (err) => {
+        console.error(err);
+        this.message.set('Error calling the adjudicate API');
+      }
+    });
+  }
 
   fetchData() {
      this.http
---
+++ frontend/src/types.ts
@@ -0,0 +1,7 @@
+export interface AdjudicateOutcome {
+  arguments_side_a: string;
+  arguments_side_b: string;
+  winner_side: string;
+  winner_probability: number;
+  compromise_solution: string;
+}
